37bdf5df1ba74d7f55a88e476afd89aaf10d4d19
FVTVLIX <andman290@gmail.com>
2021-01-07 21:15:31 -0600

    added tweenmax animations;

---
+++ index.js
@@ -0,0 +1,49 @@
+TweenMax.from(".logo", 1.6, {
+  delay: 4.4,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".menu", 1.6, {
+  delay: 4.5,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".heading", 1.6, {
+  delay: 4.6,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".description", 1.6, {
+  delay: 4.7,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from("button", 1.6, {
+  delay: 4.8,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".watch", 1.6, {
+  delay: 4.9,
+  opacity: 0,
+  y: 30,
+  rotation: 90,
+  ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".media ul li", 2, {
+  delay: 5,
+  opacity: 0,
+  y: 40,
+  ease: Expo.easeInOut
+}, 0.2);
